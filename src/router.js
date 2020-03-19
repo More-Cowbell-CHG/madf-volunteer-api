8ec6f2539763355ab11399fd5e20a45a99ad8c86
Nick Nelson <nick.nelson@chghealthcare.com>
2020-03-19 15:18:50 -0600

    add routes for each endpoint

--- src/router.js
+++ src/router.js
@@ -1,11 +1,26 @@
 const passport = require('./passport-init');
 const AuthController = require('./routes/auth.controller');
 const OfficeController = require('./routes/office.controller');
+const OpportunityController = require('./routes/opportunity.controller');
+const SignupController = require('./routes/signup.controller');
+const UserController = require('./routes/user.controller');
 
 module.exports = express => {
   const router = express.Router();
   router.post('/login', AuthController.authenticate);
   router.get('/office', auth(), OfficeController.list);
+  router.get('/opportunity', auth(), OpportunityController.list);
+  router.get('/opportunity/:id', auth(), OpportunityController.getById);
+  router.post('/opportunity', auth(), OpportunityController.create);
+  router.put('/opportunity', auth(), OpportunityController.update);
+  router.put('/opportunity/:id/set-state', auth(), OpportunityController.setState);
+  router.delete('/opportunity/:id', auth(), OpportunityController.delete);
+  router.post('/opportunity/:id/signup', auth(), SignupController.create);
+  router.delete('/opportunity/:id/signup/:start', auth(), SignupController.delete);
+  router.get('/user', auth(), UserController.list);
+  router.post('/user', auth(), UserController.create);
+  router.put('/user/:id', auth(), UserController.update);
+  router.delete('/user/:id', auth(), UserController.delete);
   return router;
 };
 
--- src/routes/user.controller.js
+++ src/routes/user.controller.js
@@ -1,6 +1,18 @@
 const Http = require('./http.util');
 
-exports.list = Http.notImplemented;
-exports.create = Http.notImplemented;
-exports.update = Http.notImplemented;
-exports.delete = Http.notImplemented;
+exports.list = (req, res) => {
+  res.send({ endpoint: 'list', success: true });
+};
+
+exports.create = (req, res) => {
+  res.send({ endpoint: 'create', success: true });
+};
+
+exports.update = (req, res) => {
+  res.send({ endpoint: 'update', success: true });
+};
+
+exports.delete = (req, res) => {
+  res.send({ endpoint: 'delete', success: true });
+};
+
